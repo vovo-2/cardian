03b5fa81d1a6410779d2d78ba0b0bbdf6b80f786
정여민 <jun1778@naver.com>
2024-01-30 13:24:40 +0900

    Feat: BE_카드혜택조회_JYM

--- BackEnd/cardian/src/main/java/A803/cardian/card/service/CardService.java
+++ BackEnd/cardian/src/main/java/A803/cardian/card/service/CardService.java
@@ -1,8 +1,10 @@
 package A803.cardian.card.service;
 
+import A803.cardian.card.data.dto.response.CardCategoryBenefitResponses;
 import A803.cardian.card.data.dto.response.MyCardListResponse;
 import A803.cardian.card.domain.MyCard;
 import A803.cardian.card.repository.CardRepository;
+import A803.cardian.card.repository.MyCardBenefitRepository;
 import A803.cardian.card.repository.MycardRepository;
 import A803.cardian.member.domain.Member;
 import A803.cardian.member.exception.MemberErrorCode;
@@ -12,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,6 +29,9 @@ public class CardService {
     @Autowired
     private final MycardRepository mycardRepository;
 
+    @Autowired
+    private final MyCardBenefitRepository myCardBenefitRepository;
+
     @Transactional
     //내 카드 전체 조회
     public MyCardListResponse findMyCards(Integer memberId) {
@@ -35,6 +42,15 @@ public class CardService {
                 .orElseThrow(() ->
                         new MemberException(MemberErrorCode.NO_MEMBER));
         return MyCardListResponse.toResponse(member);
+    }
+
+    // 내 카드의 혜택 조회 - 카드의 혜택을 List로 반환
+    // 작성자 : 정여민
+    // 작성일시 : 2024.01.30
+    @Transactional
+    public List<CardCategoryBenefitResponses> findMyCardBenefit(Integer cardId){
+        List<CardCategoryBenefitResponses> myCardBenefitList = myCardBenefitRepository.findProjectBenefitCodeByCardId(cardId);
+        return myCardBenefitList;
     }
 
 
