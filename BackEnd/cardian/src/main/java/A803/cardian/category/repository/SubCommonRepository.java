80a74e48fd909ed4c61b6cc22c866149c53eb935
yu_yeoul <yu_yeoul@naver.com>
2024-01-31 11:17:38 +0900

    Feat: BE_카테고리이미지까지조회추가_BYY

---
+++ BackEnd/cardian/src/main/java/A803/cardian/category/repository/SubCommonRepository.java
@@ -0,0 +1,8 @@
+package A803.cardian.category.repository;
+
+import A803.cardian.category.domain.SubCommonCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCommonRepository extends JpaRepository<SubCommonCode,Integer> {
+
+}
--- BackEnd/cardian/src/main/java/A803/cardian/category/service/CategoryService.java
+++ BackEnd/cardian/src/main/java/A803/cardian/category/service/CategoryService.java
@@ -1,8 +1,11 @@
 package A803.cardian.category.service;
 
 import A803.cardian.category.data.dto.reponse.CategoryList;
+import A803.cardian.category.data.dto.reponse.CategoryListImage;
+import A803.cardian.category.domain.CategoryIcon;
 import A803.cardian.category.domain.SubCommonCode;
-import A803.cardian.category.repository.CategoryRepository;
+import A803.cardian.category.repository.CategoryIconRepository;
+import A803.cardian.category.repository.SubCommonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,23 +13,42 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CategoryService {
     @Autowired
-    private final CategoryRepository categoryRepository;
-
+    private final SubCommonRepository subCommonRepository;
+    @Autowired
+    private final CategoryIconRepository categoryIconRepository;
 
     public List<CategoryList> findAllCategory(){
-        List<SubCommonCode> cate= categoryRepository.findAll();
+        List<SubCommonCode> cate= subCommonRepository.findAll();
         List<CategoryList> cateList= new ArrayList<>();
         for(SubCommonCode sub : cate){
             CategoryList category=CategoryList.builder()
                     .categoryCode(sub.getDetailCode())
                     .categoryName(sub.getName())
+                    .build();
+            cateList.add(category);
+        }
+        return cateList;
+    }
+
+    public List<CategoryListImage> findAllCategoryImage(){
+        List<SubCommonCode> cate= subCommonRepository.findAll();
+
+        List<CategoryListImage> cateList= new ArrayList<>();
+        for(SubCommonCode sub : cate){
+            String categoryCode=sub.getDetailCode();
+            String url=categoryIconRepository.findIconImageByCategoryCode(categoryCode).orElse(null);
+
+            CategoryListImage category=CategoryListImage.builder()
+                    .categoryCode(sub.getDetailCode())
+                    .categoryName(sub.getName())
+                    .categoryImage(url)
                     .build();
             cateList.add(category);
         }
