77728b030c2041d365ccb58d246c62db7fc99fd4
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-01-31 16:00:38 +0900

    Design: FE_브랜드 이미지 css 변경_MSH

--- FrontEnd/src/components/Search/Brand.tsx
+++ FrontEnd/src/components/Search/Brand.tsx
@@ -6,12 +6,12 @@ interface BrandProps {
 
 export default function Brand({categoryName, associationName, associationImage}: BrandProps) {
   return (
-    <div className="grid grid-cols-12 bg-whiteblue rounded-lg py-2 px-2 my-2">
-      <div className="col-span-2">
-        <img className="h-16 l m-auto" src={associationImage} alt={associationName} crossOrigin="anonymous"
+    <div className="flex flex-row bg-whiteblue rounded-lg py-2 px-2 my-2">
+      <div className="w-16 mr-3">
+        <img className="h-16 l m-auto rounded-full" src={associationImage} alt={associationName} crossOrigin="anonymous"
   referrerPolicy="no-referrer" />
       </div>
-      <div className="col-span-10 my-auto ml-2">
+      <div className="my-auto ml-2">
         <div className="text-sm">{categoryName}</div>
         <div className="text-lg font-semibold mt-1">{associationName}</div>
       </div>
