e0785ec31bbaec96f3fa9e3cb03e6cca578bfa2c
임소현 <vovo_2@naver.com>
2024-01-31 17:08:06 +0900

    Feat: FE_카드 혜택 UI 구현_LSH

--- FrontEnd/src/components/CardBenefit/CardBenefitList.tsx
+++ FrontEnd/src/components/CardBenefit/CardBenefitList.tsx
@@ -1,16 +1,27 @@
-import { Table } from 'flowbite-react';
-import CardBenefitListItem from './CardBenefitListItem';
+import { Accordion } from "flowbite-react";
+
+import BenefitList from "./BenefitList";
 
 export default function CardBenefitList() {
   return (
-    <div className="overflow-x-auto w-full border-whiteblue border-4 rounded-xl">
-      <Table className='w-full'>
-        <Table.Body className='divide-y divide-whiteblue'>
-          <CardBenefitListItem />
-          <CardBenefitListItem />
-          <CardBenefitListItem />
-        </Table.Body>
-      </Table>
+    <div className="overflow-auto scrollbar-hide h-[300px] ">
+      <Accordion className="border-none " collapseAll>
+        <Accordion.Panel>
+          <Accordion.Title className="bg-whiteblue w-full rounded-2xl border-none mt-2">
+            <div className="flex w-full">
+              <img src="" className="w-8 h-8 "></img>
+              <div className=" ml-5 my-auto text-5">
+                카페
+                <span className="text-2xl font-semibold">5%</span>
+                할인
+              </div>
+            </div>
+          </Accordion.Title>
+          <Accordion.Content className="border-none m-0 px-1">
+            <BenefitList />
+          </Accordion.Content>
+        </Accordion.Panel>
+      </Accordion>
     </div>
   );
 }
