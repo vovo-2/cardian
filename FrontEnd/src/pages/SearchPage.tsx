79882dda88fe95aa1b70bf22d1d8bb7f0061263d
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-01-31 15:44:25 +0900

    Refactor: FE_파일이름 변경_MSH

--- FrontEnd/src/pages/SearchPage.tsx
+++ FrontEnd/src/pages/SearchPage.tsx
@@ -1,11 +1,11 @@
 import BrandSearchBar from "../components/Search/BrandSearchBar";
-import BrandSearchResult from "../components/Search/BrandSearchResult";
+import BrandCategorySearch from "../components/Search/BrandCategorySearch";
 
 export default function Search() {
   return (
     <div>
       <BrandSearchBar />
-      <BrandSearchResult />
+      <BrandCategorySearch />
     </div>
   );
 }
