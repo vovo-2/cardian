5ec7e4297ee30a10791ab22ef9fedf26adb7329c
muuuumbi <dnpfkdlvm123@gmail.com>
2024-02-01 17:56:59 +0900

    Feat: BE_내카드상세정보 기능 추가 구현_KEB 카드 종류 (체크/신용) 출력

--- BackEnd/cardian/src/main/java/A803/cardian/card/data/dto/response/MyCardInfoDetails.java
+++ BackEnd/cardian/src/main/java/A803/cardian/card/data/dto/response/MyCardInfoDetails.java
@@ -1,9 +1,6 @@
 package A803.cardian.card.data.dto.response;
 
-import A803.cardian.card.domain.BenefitCode;
-import A803.cardian.card.domain.Card;
-import A803.cardian.card.domain.Company;
-import A803.cardian.card.domain.MyCard;
+import A803.cardian.card.domain.*;
 import A803.cardian.statistic.domain.AccumulateBenefit;
 import lombok.*;
 
@@ -17,6 +14,7 @@ public class MyCardInfoDetails {
     private String cardImage;
     private int goal;
     private int accumulate;
+    private Type type;
     private BenefitCode benefitCode;
     private int totalBenefit;
 
@@ -30,6 +28,7 @@ public class MyCardInfoDetails {
                 .cardImage(card.getImage())
                 .goal(card.getGoal())
                 .accumulate(accumulate) //누적 사용 금액
+                .type(card.getType()) //신용, 체크
                 .benefitCode(card.getBenefitCode())
                 .totalBenefit(accumulateBenefitAmount) //누적 혜택 금액
                 .build();
