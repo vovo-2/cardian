96c4b259b57580dc384f7d56d7bc84526c13884c
김민준 <alswns1285@gmail.com>
2024-02-01 14:23:31 +0900

    Feat: FE_카드 이용내역 데이터 저장_KMJ S10P12A803-165
    - 현재 URL의 parameter에서 cardId 받아옴
    - 받아온 cardId로 서버에 해당 카드 이용내역 요청 후 state에 저장

--- FrontEnd/src/components/CardTransaction/CardTransaction.tsx
+++ FrontEnd/src/components/CardTransaction/CardTransaction.tsx
@@ -1,21 +1,30 @@
-import CardTransactionList from "./CardTransactionList";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { axios } from "../../api";
+
+import CardTransactionMonthlyList from "./CardTransactionMonthlyList";
 
 export default function CardTransaction() {
+  const cardId = useParams().card_id;
+  const [transactionList, setTransactionList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/card/${cardId}/transaction`).then(({ data }) => {
+      setTransactionList(data.yearTransactionDetailsList);
+    });
+  }, [cardId]);
+
   return (
-    <div className="h-80 overflow-auto scrollbar-hide">
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
-      <CardTransactionList />
+    <div className="overflow-y-scroll  scrollbar-hide">
+      {transactionList.map(({ month, monthlyTransactionDetailsList }) => {
+        return (
+          <CardTransactionMonthlyList
+            key={month}
+            month={month}
+            monthlyTransactionDetailsList={monthlyTransactionDetailsList}
+          />
+        );
+      })}
     </div>
   );
 }
