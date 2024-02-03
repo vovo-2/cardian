5ac81207454bc86f89d7da3815d476967342a23c
임소현 <vovo_2@naver.com>
2024-02-03 23:13:53 +0900

    Feat. FE_월별 상세 소비 내역 기능 구현_LSH S10P12A803-197

--- FrontEnd/src/components/YearTransaction/YearTransactionList.tsx
+++ FrontEnd/src/components/YearTransaction/YearTransactionList.tsx
@@ -1,26 +1,81 @@
+import YearTransactionStore from "../../store/YearTransactionStore";
+
 import { Accordion } from "flowbite-react";
+import { AccordionTheme } from "../../themes/AccordionTheme";
 
 import { axios } from "../../api";
-import { useState, useEffect } from "react";
+import EntireCardTransactionStore from "./../../store/EntireCardTransactionStore";
+import CardTransactionMonthlyList from "../CardTransaction/CardTransactionMonthlyList";
+import { useEffect } from "react";
+import {
+  CardTransactionMonthlyListProps,
+  MonthlyTransactionDetails,
+} from "../../interface/CardTransactionInterface";
 
 export default function YearTransactionList() {
+  const { monthlyConsumeAmount } = YearTransactionStore();
+  const {
+    entireMonthlyTransactionList,
+    setMemberId,
+    setEntireMonthlyTransactionList,
+  } = EntireCardTransactionStore();
+
+  const member_id = 1;
+  const url = `/statistic/${member_id}/EntireCardTransaction`;
+
+  useEffect(() => {
+    axios.get(url).then((data) => {
+      setMemberId(member_id);
+
+      const arr: CardTransactionMonthlyListProps[] = [];
+      data.data.monthlyTransactionDetailsWithMonthList.map(
+        (item: {
+          month: number;
+          dailyTransactionDetailsWithDayList: MonthlyTransactionDetails[];
+        }) => {
+          const reTypeObj: CardTransactionMonthlyListProps = {
+            month: item.month,
+            monthlyTransactionDetailsList:
+              item.dailyTransactionDetailsWithDayList,
+          };
+
+          arr.push(reTypeObj);
+        }
+      );
+
+      setEntireMonthlyTransactionList(arr);
+    });
+  }, []);
+  console.log(entireMonthlyTransactionList);
   return (
-    <div>
-      <div className="overflow-y-auto scrollbar-hide  ">
-        <Accordion className="border-none w-full" collapseAll>
-          <Accordion.Panel key="" className="">
-            <Accordion.Title className="">
-              <div className="flex justify-between">
-                <div className="">1월</div>
-                <div className="">000,000원</div>
-              </div>
-            </Accordion.Title>
-            <Accordion.Content className="border-none m-0 px-1">
-              이용내역들
-            </Accordion.Content>
-          </Accordion.Panel>
-        </Accordion>
-      </div>
+    <div className="overflow-y-auto scrollbar-hide  ">
+      <Accordion theme={AccordionTheme}>
+        {monthlyConsumeAmount &&
+          monthlyConsumeAmount.map((item, idx) => (
+            <Accordion.Panel key={idx + 1}>
+              <Accordion.Title className="">
+                <div className=" ">{idx + 1}월</div>
+                <div className="">
+                  <span className="">{item}원</span>
+                </div>
+              </Accordion.Title>
+              <Accordion.Content className="border-none m-0 px-1">
+                {/* month, monthlyTransactionDetailsList */}
+                {entireMonthlyTransactionList[idx].monthlyTransactionDetailsList
+                  .length > 0 && (
+                  <CardTransactionMonthlyList
+                    key={entireMonthlyTransactionList[idx].month}
+                    month={entireMonthlyTransactionList[idx].month}
+                    monthlyTransactionDetailsList={
+                      entireMonthlyTransactionList[idx]
+                        .monthlyTransactionDetailsList
+                    }
+                  />
+                )}
+              </Accordion.Content>
+            </Accordion.Panel>
+          ))}
+      </Accordion>
     </div>
   );
 }
