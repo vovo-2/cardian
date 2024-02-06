207762031edff7604bf1ff28c376479a52b6b0e0
yu_yeoul <yu_yeoul@naver.com>
2024-02-06 14:46:16 +0900

    Fix: BE_컨트롤러 설명 추가_BYY -category,settlement컨트롤러 설명 추가

--- BackEnd/cardian/src/main/java/A803/cardian/category/controller/CategoryController.java
+++ BackEnd/cardian/src/main/java/A803/cardian/category/controller/CategoryController.java
@@ -5,6 +5,7 @@ import A803.cardian.associate.data.dto.response.Search;
 import A803.cardian.associate.service.AssociateService;
 import A803.cardian.category.data.dto.reponse.CategoryList;
 import A803.cardian.category.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,30 +21,30 @@ public class CategoryController {
     private final CategoryService categoryService;
     @Autowired
     private final AssociateService associateService;
-
+    @Operation(summary = "카테고리 조회", description = "카테고리 리스트 출력")
     @GetMapping("/category")
     public ResponseEntity<?> getCategories(){
 
         return ResponseEntity.ok(categoryService.findAllCategory());
     }
-
+    @Operation(summary = "카테고리 별 제휴사 조회", description = "카테고리 별 제휴사 리스트 출력")
     @GetMapping("/{category_id}/association")
     public ResponseEntity<?> getSelectCategory(@PathVariable("category_id") String categoryCode){
 
         return ResponseEntity.ok(associateService.findAssociation(categoryCode));
     }
-
+    @Operation(summary = "카테고리+이미지 조회", description = "카테고리 목록+이미지 출력")
     @GetMapping("/category-image")
     public ResponseEntity<?> getCategoryImage(){
 
         return ResponseEntity.ok(categoryService.findAllCategoryImage());
     }
-
+    @Operation(summary = "제휴사 검색", description = "제휴사 검색")
     @PostMapping("/association")
     public ResponseEntity<?> searchAssociation(@RequestBody Search search){
         return ResponseEntity.ok(associateService.searchAssociation(search));
     }
-
+    @Operation(summary = "선택한 제휴사 별 카드 추천", description = "선택한 제휴사에 대해 가장 좋은 혜택을 가진 카드부터 출력")
     @GetMapping("/association/card-list")
     public ResponseEntity<?> categoryCardRecommend(@RequestParam Integer memberId,@RequestParam Integer associateId){
 
--- BackEnd/cardian/src/main/java/A803/cardian/settlement/controller/SettlementController.java
+++ BackEnd/cardian/src/main/java/A803/cardian/settlement/controller/SettlementController.java
@@ -3,6 +3,7 @@ package A803.cardian.settlement.controller;
 import A803.cardian.goal.service.GoalService;
 import A803.cardian.settlement.data.dto.response.SalaryUpdate;
 import A803.cardian.settlement.service.SettlementService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,30 +19,31 @@ public class SettlementController {
     private final  SettlementService settlementService;
     @Autowired
     private final GoalService goalService;
+    @Operation(summary = "내 연봉 조회", description = "내 연봉 조회")
     @GetMapping("/{member_id}")
     public ResponseEntity<?> getMySalary(@PathVariable("member_id") Integer member_id){
 
         return ResponseEntity.ok(settlementService.findMySalary(member_id));
     }
-
+    @Operation(summary = "내 연봉 정보 수정", description = "내 연봉 정보 수정")
     @PutMapping("/salary")
     public void updateSalary(@RequestBody SalaryUpdate salaryUpdate){
 
         settlementService.updateSalary(salaryUpdate.getMember_id(),salaryUpdate.getSalary());
     }
-
+    @Operation(summary = "연말정산 기준 달성 여부 조회", description = "달성시 true, 미달성시 false")
     @GetMapping("/achievement-standard")
     public ResponseEntity<?> getAchieve(@RequestParam Integer memberId){
 
         return ResponseEntity.ok(goalService.getAchieve(memberId));
     }
-
+    @Operation(summary = "연말정산 기준 미달성", description = "연말정산 기준 미달성시 보내줄 데이터")
     @GetMapping("/not-achievement")
     public ResponseEntity<?> getNotAchieveSettlement(@RequestParam Integer memberId){
 
         return ResponseEntity.ok(settlementService.settlementNotAchieve(memberId));
     }
-
+    @Operation(summary = "연말정산 기준 달성", description = "연말정산 기준 달성시 보내줄 데이터")
     @GetMapping("/achievement")
     public ResponseEntity<?> getAchieveSettlement(@RequestParam Integer memberId){
 
