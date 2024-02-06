a817f05ec408ee3d48ee4da5d2cd5101f1d4c17a
muuuumbi <dnpfkdlvm123@gmail.com>
2024-02-06 15:05:47 +0900

    Feat: BE_api 경로, 파라미터  수정_KEB

--- BackEnd/cardian/src/main/java/A803/cardian/settlement/controller/SettlementController.java
+++ BackEnd/cardian/src/main/java/A803/cardian/settlement/controller/SettlementController.java
@@ -15,37 +15,40 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class SettlementController {
-    @Autowired
     private final  SettlementService settlementService;
-    @Autowired
     private final GoalService goalService;
+
     @Operation(summary = "내 연봉 조회", description = "내 연봉 조회")
     @GetMapping("/{member_id}")
     public ResponseEntity<?> getMySalary(@PathVariable("member_id") Integer member_id){
 
         return ResponseEntity.ok(settlementService.findMySalary(member_id));
     }
-    @Operation(summary = "내 연봉 정보 수정", description = "내 연봉 정보 수정")
-    @PutMapping("/salary")
-    public void updateSalary(@RequestBody SalaryUpdate salaryUpdate){
 
-        settlementService.updateSalary(salaryUpdate.getMember_id(),salaryUpdate.getSalary());
+    @Operation(summary = "내 연봉 정보 수정", description = "내 연봉 정보 수정")
+    @PutMapping("/{member_id}/{salary}")
+    public void updateSalary(@PathVariable("member_id")int memberId, @PathVariable("salary")int salary){
+
+        settlementService.updateSalary(memberId,salary);
     }
+
     @Operation(summary = "연말정산 기준 달성 여부 조회", description = "달성시 true, 미달성시 false")
-    @GetMapping("/achievement-standard")
-    public ResponseEntity<?> getAchieve(@RequestParam Integer memberId){
+    @GetMapping("/{member_id}/achievement-standard")
+    public ResponseEntity<?> getAchieve(@PathVariable int memberId){
 
         return ResponseEntity.ok(goalService.getAchieve(memberId));
     }
+
     @Operation(summary = "연말정산 기준 미달성", description = "연말정산 기준 미달성시 보내줄 데이터")
-    @GetMapping("/not-achievement")
-    public ResponseEntity<?> getNotAchieveSettlement(@RequestParam Integer memberId){
+    @GetMapping("/{member_id}/not-achievement")
+    public ResponseEntity<?> getNotAchieveSettlement(@PathVariable("member_id")int memberId){
 
         return ResponseEntity.ok(settlementService.settlementNotAchieve(memberId));
     }
+
     @Operation(summary = "연말정산 기준 달성", description = "연말정산 기준 달성시 보내줄 데이터")
-    @GetMapping("/achievement")
-    public ResponseEntity<?> getAchieveSettlement(@RequestParam Integer memberId){
+    @GetMapping("/{member_id}/achievement")
+    public ResponseEntity<?> getAchieveSettlement(@PathVariable("member_id")int memberId){
 
         return ResponseEntity.ok(settlementService.settlementAchieve(memberId));
     }
