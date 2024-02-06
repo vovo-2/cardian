a637a3d8bba0dbbfe00d6dc027562bf2177d980b
yu_yeoul <yu_yeoul@naver.com>
2024-02-06 09:15:03 +0900

    Fix: BE_webconfig설정_BYY' -요청방식 추가

--- BackEnd/cardian/src/main/java/A803/cardian/config/WebConfig.java
+++ BackEnd/cardian/src/main/java/A803/cardian/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:5173", "http://i10a803.p.ssafy.io", "http://i10a803.p.ssafy.io:80", "http://i10a803.p.ssafy.io:443")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS");
     }
 }
